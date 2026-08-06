df865645ebb84ee84ebb582c74ba71acd1bdac47
Austin R Hudson <hudsonaustin25@gmail.com>
2026-08-05 22:37:37 -0500

    Fixed bugs and file extension in WorkerController, application works now

--- src/main/java/com/minicloud/sampo/WorkerController.java
+++ src/main/java/com/minicloud/sampo/WorkerController.java
@@ -1,15 +1,24 @@
 package com.minicloud.sampo;
 
-import java.util.concurrent.atomic.AtomicLong;
+import com.Worker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/workers")
 public class WorkerController {
-    private final workerService workerService;
+    private final WorkerService workerService;
+
+    public WorkerController(WorkerService workerService) {
+        this.workerService = workerService;
+    }
 
     @PostMapping("/register")
     public void registerWorker(@RequestBody Worker worker) {
@@ -20,3 +29,4 @@ public class WorkerController {
     public Map<String, Worker> listWorkers() {
         return workerService.getWorkers();
     }
+}
